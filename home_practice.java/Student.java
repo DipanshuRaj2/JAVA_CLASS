0465813ee34fd1438e77d23c8806e05ac8326067
Dipanshu Raj <dipanshurajjmt2@gmail.com>
2023-01-31 00:02:40 +0530

    understanding_class

--- home_practice.java/Student.java
+++ home_practice.java/Student.java
@@ -6,5 +6,7 @@ class Student{
         s1 =new     Student(); //creating reference to object of Student
         System.out.println(s1.id); //accessing member through reference variable
         System.out.println(s1.name);  
+        //0
+        //null
     }
 }
