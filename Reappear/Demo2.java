6b26d2708ac0b5e5dfcc49aeeba941fc2af11550
dip <dipanshurajjmt2@gmail.com>
2023-10-19 01:04:45 +0530

    meassge

---
+++ Reappear/Demo2.java
@@ -0,0 +1,19 @@
+class Calculator{
+    int a;
+    public int add(){
+        System.out.println("good night");
+        return 0;
+    }
+}
+class Demo2{
+public static void main(String args[]){
+
+
+    
+    
+    
+
+
+    
+}
+} 
