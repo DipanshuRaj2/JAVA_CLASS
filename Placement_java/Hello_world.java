35f14c30be82c20949fc49c1bbc9cc83977fd7e3
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-15 14:24:37 +0530

    javq

---
+++ Placement_java/Hello_world.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Hello_world{
+    public static void main(String[]args){
+        int a =4;
+        int b =5;
+        
+        System.out.println("a+b: "+(a+b));
+    }
+}
