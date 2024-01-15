b6a8a729c4613864a60f43360ee69c3ebd3b8239
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-15 16:44:28 +0530

    message

--- Placement_java/Hello_world.java
+++ Placement_java/Hello_world.java
@@ -1,9 +1,71 @@
-import java.util.*;
+import java.util.Scanner;
 class Hello_world{
     public static void main(String[]args){
-        int a =4;
-        int b =5;
         
-        System.out.println("a+b: "+(a+b));
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the value of a: ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter the value of b: ");
+
+
+        // int b = sc.nextInt();
+        
+
+        // System.out.println("a+b: "+(a+b));
+
+        
+        
+
+
+        // String name =sc.nextLine();
+        
+
+        // System.out.println();
+
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the value of a");
+
+        // int a = sc.nextInt();
+
+        // System.out.println("Enter the value of b");
+
+        // int b = sc.nextInt();
+
+        // System.out.println("a : "+a+" b : "+b);
+        // if(a < b){
+        //     System.out.println("b is greater");
+        // }
+        // else{
+        //     System.out.println("a is greater");
+        // }
+
+        // int temp = 0;
+
+        // temp = a;
+        // a  = b;
+        // b = temp;
+        // System.out.println("a : "+a+" b : "+b);
+
+        //using two varible
+        // a = a+b;
+        // b = a-b;
+        // a = a-b;
+        // System.out.println("a : "+a+" b : "+b);
+        // System.out.println("Enter the value of n");
+        // int n =sc.nextInt();
+
+        // for(int i = 1; i<=n; i++){
+        //     System.out.println("i = "+i);
+        // }
+        
+        int a = 0;
+        int b = 1;
+        cout<<a<<" "<<b; 
+        
+        
     }
 }
